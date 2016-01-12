d203d9ae6618b9f33aaa6e2b47596d41a15a00eb
Tom Jaspers <info@javaminor2015.com>
2016-01-12 10:30:49 +0100

    Package fix + sonar plugin

--- app/src/main/java/com/kantilever/t1c3android/activities/MainActivity.java
+++ app/src/main/java/com/kantilever/t1c3android/activities/MainActivity.java
@@ -1,13 +1,15 @@
-package com.pizza.tom.t1c3android;
+package com.kantilever.t1c3android.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.kantilever.t1c3android.R;
 
 public class MainActivity extends AppCompatActivity {
 
