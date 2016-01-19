0b2f9e09a421f08af54a6850431f4b81a5948fed
Tom Jaspers <info@javaminor2015.com>
2016-01-19 15:26:16 +0100

    Dont use gsonconverter anymore

--- app/src/main/java/com/kantilever/t1c3android/rest/services/abs/AbsClient.java
+++ app/src/main/java/com/kantilever/t1c3android/rest/services/abs/AbsClient.java
@@ -1,21 +1,11 @@
 package com.kantilever.t1c3android.rest.services.abs;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.kantilever.t1c3android.converter.deserializers.AddressDeserializer;
-import com.kantilever.t1c3android.converter.deserializers.CustomerDeserializer;
-import com.kantilever.t1c3android.converter.deserializers.OrderDeserializer;
-import com.kantilever.t1c3android.domain.Address;
-import com.kantilever.t1c3android.domain.Customer;
-import com.kantilever.t1c3android.domain.rest.CustomerOrder;
-
 import java.io.IOException;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
 
 /**
@@ -23,17 +13,14 @@ import retrofit2.Retrofit;
  */
 public abstract class AbsClient {
 
-    private static final Gson GSON = new GsonBuilder().registerTypeAdapter(Customer.class, new CustomerDeserializer())
-                                                      .registerTypeAdapter(CustomerOrder.class, new OrderDeserializer())
-                                                      .registerTypeAdapter(Address.class, new AddressDeserializer())
-                                                      .setPrettyPrinting()
-                                                      .create();
-
     /**
      * The constant BASE_URL.
      */
     protected static final String BASE_URL = "http://10.32.40.234:6789";
 
+    /**
+     * The constant HttpClient
+     */
     protected static OkHttpClient client;
     /**
      * The constant REST_ADAPTER.
@@ -57,7 +44,6 @@ public abstract class AbsClient {
 
         REST_ADAPTER = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(GSON))
                 .client(client)
                 .build();
     }
